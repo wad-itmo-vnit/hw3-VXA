ac168978f088780dc7fc9f24d95a68676a05893f
xuananh95 <60329143+xuananh95@users.noreply.github.com>
2021-07-31 13:29:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-app.run(host="localhost", port=5000)
