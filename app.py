6e10f1cfd1351d8e0518daa7672964a8ee0367f9
xuananh95 <xanh.trym95@gmail.com>
2021-08-06 16:57:47 +0700

    added image route

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/img/<number>')
+def image(number):
+    filename = './static/images/dota' + str(number) + '.jpg'
+    return send_file(filename)
 
 app.run(host="localhost", port=5000)
